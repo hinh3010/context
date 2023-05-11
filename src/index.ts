beefacea3a09391dcaab0327bab569819aca5be3
hinhtv <xuanhinh301099@gmail.com>
2023-05-11 22:54:01 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+export interface IContext {
+  mongodb: {
+    uri: string;
+    options: {
+      useNewUrlParser?: boolean;
+      useUnifiedTopology?: boolean;
+      ssl?: boolean;
+      sslValidate?: boolean;
+      socketTimeoutMS?: number;
+      connectTimeoutMS?: number;
+      serverSelectionTimeoutMS?: number;
+      dbName?: string;
+    };
+  };
+  redisDb: {
+    uri: string;
+  };
+}
